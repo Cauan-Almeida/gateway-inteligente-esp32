f38bd0658192a6a7ccfa08035b3b97e8f0721e1c
Cauan Almeida <cauan.almeida@example.com>
2026-06-10 20:02:37 -0300

    Adiciona codigo de factory reset no utils

--- utils/factory_reset.ino
+++ utils/factory_reset.ino
@@ -1,8 +1,32 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <RoboCore_SMW_SX1276M0.h>
+
+HardwareSerial LoRaSerial(2);
+#define RXD2 16
+#define TXD2 17
+
+SMW_SX1276M0 lorawan(LoRaSerial);
+
 void setup() {
   Serial.begin(115200);
-  // Código de Factory Reset AT+FRESET
-  Serial.println("Executando Factory Reset do LoRa...");
+  delay(500);
+  LoRaSerial.begin(115200, SERIAL_8N1, RXD2, TXD2);
+
+  lorawan.setPinReset(5);
+  lorawan.reset();
+
+  Serial.println("Fazendo factory reset...");
+  delay(2000);
+
+  LoRaSerial.println("AT+FRESET");
+  delay(5000);
+
+  while (LoRaSerial.available()) {
+    Serial.write(LoRaSerial.read());
+  }
+
+  Serial.println("Pronto!");
 }
 
-void loop() {
-}
+void loop() {}
